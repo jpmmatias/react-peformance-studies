e26c79c403664986e5c65b35d449282aa1755e48
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-23 09:41:12 -0300

    Memo

--- pages/components/ProductItem.tsx
+++ pages/components/ProductItem.tsx
@@ -1,10 +1,12 @@
+import { memo } from 'react';
+
 type product = {
 	id: number;
 	price: number;
 	title: string;
 };
 
-const ProductItem = (product: product) => {
+const ProductItemComponent = (product: product) => {
 	return (
 		<li>
 			{product.title} - <strong>{product.price}</strong>
@@ -12,4 +14,22 @@ const ProductItem = (product: product) => {
 	);
 };
 
+const ProductItem = memo(ProductItemComponent, (prevProduct, nextProduct) => {
+	return (
+		prevProduct.id === nextProduct.id &&
+		prevProduct.price === nextProduct.price &&
+		prevProduct.title === nextProduct.title
+	);
+});
+
 export default ProductItem;
+
+// O que o react faz:
+// 1 - Cria uma nova versão do componenete
+// 2 - Compara com a versão anterior
+// 3 - Se houverem alterações, vai atualizar o que alterou
+
+// Quando usar Memo
+// 1 - Componentes funcionais puros -> Quando componente com os mesmos inputs, volta com o mesmo resultado
+// 2 -  Renderizam demais
+// 3 - Compnentes grandes
